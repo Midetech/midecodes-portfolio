f76765da63d0759663d2cfec1e7dd80b1921c35d
Midecodes <faginorish@gmail.com>
2024-07-11 23:51:45 +0100

    Update projects

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -13,13 +13,13 @@ const Projects = () => {
 
       <div className="flex lg:gap-[21px] gap-y-[21px] flex-wrap">
         <a target="_blank" href="https://mygift.myserviceagent.net/">
-          <div className="lg:w-[538px] lg:h-[538px] bg-[#C3D5E6] rounded-[15px] lg:px-20 px-10 lg:pt-[53px] pt-10 overflow-hidden relative">
+          <div className="lg:w-[538px] lg:h-[538px] lg:bg-[#C3D5E6] rounded-[15px] lg:px-20 px-10 lg:pt-[53px] pt-10 overflow-hidden relative">
             <img src="/images/mygift.png" alt="mygift" />
             <BorderBeam size={250} duration={12} delay={9} />
           </div>
         </a>
         <a target="_blank" href="https://purplesale.finance/">
-          <div className="lg:w-[769px] lg:h-[538px] bg-[#FFA3B0] rounded-[15px] lg:px-20 px-10  lg:pt-[53px] pt-10 overflow-hidden relative ">
+          <div className="lg:w-[769px] lg:h-[538px] lg:bg-[#FFA3B0] rounded-[15px] lg:px-20 px-10  lg:pt-[53px] pt-10 overflow-hidden relative ">
             <img
               src="/images/purplesale.png"
               alt="purplesale"
@@ -29,13 +29,13 @@ const Projects = () => {
           </div>
         </a>
         <a target="_blank" href="https://mygift.myserviceagent.net/">
-          <div className="lg:w-[769px] lg:h-[538px] bg-[#FEDB16] rounded-[15px]  lg:px-20 px-10 lg:pt-[53px] pt-10 overflow-hidden relative">
+          <div className="lg:w-[769px] lg:h-[538px] lg:bg-[#FEDB16] rounded-[15px]  lg:px-20 px-10 lg:pt-[53px] pt-10 overflow-hidden relative">
             <img src="/images/alienchain.png" alt="mygift" />
             <BorderBeam size={250} duration={12} delay={9} />
           </div>
         </a>
         <a target="_blank" href="https://myserviceagent.net/">
-          <div className="lg:w-[538px] lg:h-[538px] bg-[#EB2931] rounded-[15px] lg:px-20 px-10 lg:pt-[53px] pt-10 overflow-hidden relative">
+          <div className="lg:w-[538px] lg:h-[538px] lg:bg-[#EB2931] rounded-[15px] lg:px-20 px-10 lg:pt-[53px] pt-10 overflow-hidden relative">
             <img src="/images/myserviceagent.png" alt="myserviceagent" />
             <BorderBeam size={250} duration={12} delay={9} />
           </div>
