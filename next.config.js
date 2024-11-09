cd046a6f5b756833e8a97dcd40cf92f81b66cac2
Midecodes <faginorish@gmail.com>
2024-11-09 16:51:20 +0100

    Update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   images: {
+    unoptimized: true,
     domains: ["res.cloudinary.com"], // Add your image hostname(s) here
   },
 };
