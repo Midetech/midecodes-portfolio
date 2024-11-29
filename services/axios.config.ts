cdc8c2f8c094f0bf1f489317ccc67b210043844d
Midecodes <faginorish@gmail.com>
2024-11-29 10:43:31 +0100

    update

--- services/axios.config.ts
+++ services/axios.config.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://mide.codes/api',
+    baseURL: 'https://mide.codes/api',
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json',
